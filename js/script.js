26dc50b78cfeee6c5f25ab93af4101dde86426a3
Tatiana Nikonova <81408729+Tatiana-Nikonova@users.noreply.github.com>
2021-03-29 22:12:12 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -63,6 +63,18 @@ if (animItems.length > 0) {
 	}, 300);
 }
 
+// Анимация появления снизу---end
+
+// Споллеры
+$(document).ready(function() {
+	$('.block__title').click(function(event) {
+		if($('.block').hasClass('one')) {
+			$('.block__title').not($(this)).removeClass('active');
+			$('.block__text').not($(this).next()).slideUp(300);
+		}
+		$(this).toggleClass('active').next().slideToggle(300);
+	});
+});
 
 
 
